2453d65be5d5de85e6c244dcc4372c0a65db6871
Hacking in sneakers <sneakerhax@gmail.com>
2015-01-06 09:54:04 -0800

    0.1 First commit :)

---
+++ python-network-tools.py
@@ -0,0 +1,31 @@
+import sys
+import curl
+ 
+def banner():
+	print "#################################"
+	print "##     python network tools    ##"
+	print "#################################"
+ 
+def usage():
+	print "How to use network curl:"
+	print "--curl        - curl with pycurl lib"
+	print "--urllib_curl - curl with urllib2 lib"
+ 
+	print "examples:"
+	print "curl.py --curl www.website.com"
+	print "curl.py --urllib_curl http:\\website.com"
+	
+	
+	
+if __name__ == "__main__":
+	banner()    
+	if sys.argv[1] == "--curl":
+		print "curling website..."
+		curl.curl(sys.argv[2])
+	elif sys.argv[1] == "--urllib_curl":
+		print "curling website..."
+		curl.urllib_curl(sys.argv[2])
+	elif sys.argv[1] == "--help":
+		usage()
+	else:
+		usage()
