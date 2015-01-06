9b33c4b4cd386b4019173b49212ec202a0ecda46
Hacking in sneakers <sneakerhax@gmail.com>
2015-01-06 12:20:06 -0800

    Fixed mx_lookup so it can print out multiple MX records

--- resolve.py
+++ resolve.py
@@ -1,7 +1,7 @@
 from dns import resolver
-import sys
 
 def mx_lookup(domain, *args):
-	mx = resolver.query(sys.argv[2], 'MX')
-	print mx[0]
+	mx = resolver.query(domain, 'MX')
+	for record in mx:
+		print record
 	
