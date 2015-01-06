59ddfbc76d594cac969f82d65397e9212051586c
Hacking in sneakers <sneakerhax@gmail.com>
2015-01-06 13:03:31 -0800

    Added MX record lookup to argparse

--- python-network-tools.py
+++ python-network-tools.py
@@ -9,24 +9,19 @@ def banner():
     print "#################################"
 
 
-def usage():
-    print "How to use python network tools:"
-    print "--curl        - curl with pycurl lib"
-    print "--urllib_curl - curl with urllib2 lib"
-
-    print "examples:"
-    print "curl.py --curl www.website.com"
-    print "curl.py --urllib_curl http:\\website.com"
-
-
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Python Network Tools')
-    parser.add_argument('-c', help='Curl a provided URL with pycurl')
-    parser.add_argument('--urllibcurl', help='Curl a provided url with urllib')
-    args = parser.parse_args()
-    if args.c:
-        result = curl.curl(args.c)
-        print result
-    if args.urllibcurl:
-        result = curl.urllib_curl(args.urllibcurl)
-        print result
+	parser = argparse.ArgumentParser(description='Python Network Tools')
+	parser.add_argument('-curl', help='Curl a provided URL with pycurl')
+	parser.add_argument('-urllibcurl', help='Curl a provided url with urllib')
+	parser.add_argument('-mx', help='Lookup MX record')
+	args = parser.parse_args()
+
+if args.curl:
+	result = curl.curl(args.c)
+	print result
+if args.urllibcurl:
+	result = curl.urllib_curl(args.urllibcurl)
+	print result
+if args.mx:
+	result = resolve.mx_lookup(args.mx)
+	print result
--- resolve.py
+++ resolve.py
@@ -1,7 +1,11 @@
 from dns import resolver
+import socket
 
-def mx_lookup(domain, *args):
+def mx_lookup(domain):
 	mx = resolver.query(domain, 'MX')
 	for record in mx:
 		print record
-	
+		
+def resolve_hostname(hostname):
+	host = socket.gethostbyname(hostname)
+	print host
