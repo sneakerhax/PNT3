ec3f9462e66c9ac0ac4a7daf59ecab383771c924
Hacking in sneakers <sneakerhax@gmail.com>
2015-01-27 20:44:30 -0800

    Added dnsreverse and other updates

--- python-network-tools.py
+++ python-network-tools.py
@@ -8,12 +8,13 @@ def banner():
     print "##     python network tools    ##"
     print "#################################"
 
-
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='Python Network Tools')
 	parser.add_argument('--curl', help='Curl a provided URL with pycurl')
 	parser.add_argument('--ucurl', help='Curl a provided URL with urllib')
 	parser.add_argument('--mx', help='Lookup MX record')
+	parser.add_argument('--dnsresolve', help='resolve hostname')
+	parser.add_argument('--dnsreverse', help='reverse lookup')
 	args = parser.parse_args()
 	
 banner()
@@ -25,4 +26,10 @@ if args.ucurl:
 	print result
 if args.mx:
 	result = resolve.mx_lookup(args.mx)
+	print result
+if args.dnsresolve:
+	result = resolve.resolve_hostname(args.dnsresolve)
+	print result
+if args.dnsreverse:
+	result = resolve.reverse_lookup(args.dnsreverse)
 	print result
--- resolve.py
+++ resolve.py
@@ -1,11 +1,18 @@
 from dns import resolver
 import socket
 
+#MX lookup
 def mx_lookup(domain):
 	mx = resolver.query(domain, 'MX')
 	for record in mx:
 		print record
-		
+
+#DNS lookup		
 def resolve_hostname(hostname):
-	host = socket.gethostbyname(hostname)
-	print host
+	ip = socket.gethostbyname(hostname)
+	return ip
+	
+#Reverse lookup
+def reverse_lookup(ip):
+	hostname = socket.gethostbyaddr(ip)
+	return hostname[0]
