e988cce4a696cf9665041212392e021d7990b668
Hacking in sneakers <sneakerhax@gmail.com>
2015-07-07 15:16:01 -0700

    Adding wlan_password_clear.py

---
+++ wlan_password_clear.py
@@ -0,0 +1,16 @@
+import os
+import sys 
+
+usage = "usage: wlan_password_clear.py <ssid>"
+
+def wlanclear(ssid):
+	cmd = 'netsh wlan show profile name='+ssid+' key=clear | findstr "Key Content"'
+	results = os.system(cmd)
+	return results
+	
+if len(sys.argv) == 2:
+	ssid = sys.argv[1]
+	wlanclear(ssid)	
+else:
+	print usage
+	
