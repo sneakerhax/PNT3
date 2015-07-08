e3d8b8a606ef272d2f8e63f1047fbc13178d16cb
Hacking in sneakers <sneakerhax@gmail.com>
2015-07-08 15:38:30 -0700

    update to smtp_authcheck.py

--- smtp_authcheck.py
+++ smtp_authcheck.py
@@ -1,25 +1,46 @@
 import smtplib
 import sys, argparse
 
-def is_authenticated(server, port, username, password):
+def main(args):	
+	try:
+		print smtp_auth(args.server, args.port, args.username, args.password)
+	except:
+		usage()
+
+def banner():
+	print "#################################"
+	print "#       smtp_authcheck.py       #"
+	print "#        by: sneakerhax         #"
+	print "#################################"
+
+def usage():
+	print "Usage: python smtp_authcheck.py --server <mail server> --port <SMTP port> --username <username> --password <password>"
+
+def smtp_auth(server, port, username, password):
 	s = smtplib.SMTP(server, port)
 	s.starttls()
 
 	try:
 		s.login(username, password)
-		return "Accepted username and password"
+		return "Authentication successful"
 
 	except smtplib.SMTPAuthenticationError:
 		return "Wrong username or password"
 
 if __name__ == '__main__':
-
+	
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--server", action="store", dest='server', help="server address")
 	parser.add_argument("--port", action="store", dest='port', help="server port", type=int)
 	parser.add_argument("--username", action="store", dest='username', help="username")
 	parser.add_argument("--password", action="store", dest='password', help="password")
 
-	results = parser.parse_args()
+	args = parser.parse_args()
 	
-	print is_authenticated(results.server, results.port, results.username, results.password)
+	print "\n"
+	banner()
+	main(args)
+
+	
+	
+	
