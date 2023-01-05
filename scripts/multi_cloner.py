60d741bb7fdf885dbb754cabafd4c373dff39c12
sneakerhax <sneakerhax@gmail.com>
2023-01-04 17:29:54 -0800

    update multi_cloner

--- scripts/multi_cloner.py
+++ scripts/multi_cloner.py
@@ -1,32 +1,33 @@
 import sys
 import subprocess
 
+usage = "[*] Usage: python3 multi_cloner.py <repo_list>"
+
 
 def banner():
-    print("")
-    print("#######################")
-    print("#    Multi Cloner     #")
-    print("#######################")
+    print("#################################")
+    print("#       multi_cloner.py         #")
+    print("#        by: sneakerhax         #")
+    print("#################################")
     print("")
 
 
-def clone(git_url, repo_list):
+def clone(repo_list):
     with open(repo_list, 'r') as clone_list:
         for repo in clone_list:
-            print("[+] Cloning repo " + str(git_url) + str(repo))
-            remote_origin = str(git_url + repo).strip()
+            print("[+] Cloning repo " + str(repo.strip()))
+            remote_origin = str(repo).strip()
             subprocess.Popen(["git", "clone", remote_origin], stdout=subprocess.PIPE).communicate()[0]
             print("\n")
 
 
 def main():
-    if len(sys.argv) == 3:
+    if len(sys.argv) == 2:
+        repo_list = sys.argv[1]
         banner()
-        git_url = sys.argv[1]
-        repo_list = sys.argv[2]
-        clone(git_url, repo_list)
+        clone(repo_list)
     else:
-        print("[*] Usage: python3 multi_cloner.py <repo_url> <repo_list>")
+        print(usage)
 
 
 if __name__ == "__main__":
