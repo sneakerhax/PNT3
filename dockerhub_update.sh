68991e956a87770800994df7970c12ef6570bf7a
sneakerhax <sneakerhax@gmail.com>
2025-01-29 09:03:58 -0800

    Updated dockerhub_updates.sh to use buildx

--- dockerhub_update.sh
+++ dockerhub_update.sh
@@ -1,3 +1,3 @@
 # build and push PNT3 to Docker Hub
-docker build -t sneakerhax/pnt3 -f ./Dockerfile .
-docker push sneakerhax/pnt3:latest
+docker buildx build --platform linux/amd64 -t sneakerhax/pnt3 -f ./Dockerfile . --push
+# docker push sneakerhax/pnt3:latest
